d0bd24581ecb8dcb2567289160d6f33463205cdd
heartggs <jounghj0806@naver.com>
2025-06-13 13:35:10 +0900

    feat:신용점수 컴포넌트

--- src/components/home/account.tsx
+++ src/components/home/account.tsx
@@ -5,18 +5,22 @@ import Text from '@shared/Text'
 import Button from '@shared/Button'
 import Spacing from '@shared/Spacing'
 
-function Account(){
+function Account() {
   const hasAccount = true
 
   // 계좌를 보유중일때
-  if(hasAccount){
-    return(
-      <div style={{padding:24}}>
-        <Flex justify='space-betwen' align='center'>
-          <Flex direction='column'>
-            <Text typography='t6' color='gray600'>올라프 회원님의 자산</Text>
-            <Spacing size={2}/>
-            <Text typography='t3' bold={true}>7,000원</Text>
+  if (hasAccount) {
+    return (
+      <div style={{ padding: 24 }}>
+        <Flex justify="space-between" align="center">
+          <Flex direction="column">
+            <Text typography="t6" color="gray600">
+              올라프 회원님의 자산
+            </Text>
+            <Spacing size={2} />
+            <Text typography="t3" bold={true}>
+              7,000원
+            </Text>
           </Flex>
           <Button>분석</Button>
         </Flex>
@@ -29,25 +33,29 @@ function Account(){
 
   // READY | DONE
   const 계좌개설상태 = 'READY'
-  const title = 계좌개설상태 == 'READY' ? "만들고 있으신\n계좌가 있으시군요":"계좌개설이\n더 쉽고 빨라졌어요"
-  const buttonLabel = 계좌개설상태 === 'READY' ? '이어만들기' :'3분만에 개설하기'
+  const title =
+    계좌개설상태 == 'READY'
+      ? '만들고 있으신\n계좌가 있으시군요'
+      : '계좌개설이\n더 쉽고 빨라졌어요'
+  const buttonLabel =
+    계좌개설상태 === 'READY' ? '이어만들기' : '3분만에 개설하기'
   return (
-    <div style={{padding:24}}>
-      <Flex justify='space-between'>
-      <Flex direction='column'>
-        <Text bold={true} style={{whiteSpace:'pre-wrap'}}>
-          {title}
-        </Text>
-        <Spacing size={8}/>
-        <Button>{buttonLabel}</Button>
+    <div style={{ padding: 24 }}>
+      <Flex justify="space-between">
+        <Flex direction="column">
+          <Text bold={true} style={{ whiteSpace: 'pre-wrap' }}>
+            {title}
+          </Text>
+          <Spacing size={8} />
+          <Button>{buttonLabel}</Button>
+        </Flex>
+        <Image
+          src="https://cdn2.iconfinder.com/data/icons/money-related/128/MONEY_2-05-512.png"
+          alt=""
+          width={80}
+          height={80}
+        />
       </Flex>
-      <Image 
-        src="https://cdn2.iconfinder.com/data/icons/money-related/128/MONEY_2-05-512.png"
-        alt=""
-        width={80}
-        height={80}
-      />
-    </Flex>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,26 @@
 import dynamic from 'next/dynamic'
-import styled from '@emotion/styled'
-import { css } from '@emotion/react'
-import Skeleton from '@/components/shared/Skeleton'
 import Account from '@components/home/account'
 import { BannerSkeleton } from '@/components/home/EventBanners'
+import { CreditScoreSkeleton } from '@/components/home/CreditScore'
+import Spacing from '@/components/shared/Spacing'
 
+const EventBanners = dynamic(() => import('@/components/home/EventBanners'), {
+  ssr: false,
+  loading: () => <BannerSkeleton />,
+})
 
-const EventBanners = dynamic(() => import('@/components/home/EventBanners'),{
-  ssr:false,
-  loading :()=> (
-    <BannerSkeleton/>
-  )
-}) 
+const CreditScore = dynamic(() => import('@/components/home/CreditScore'), {
+  ssr: false,
+  loading: () => <CreditScoreSkeleton />,
+})
 
 export default function Home() {
   return (
     <>
-      <EventBanners/>
-      <Account/>
+      <EventBanners />
+      <Account />
+      <Spacing size={8} backgroundColor="gray100" />
+      <CreditScore />
     </>
   )
 }
